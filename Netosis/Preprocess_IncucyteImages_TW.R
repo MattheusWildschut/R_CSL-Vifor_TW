874be50ff106eb66968c3c988ab3ecf8034afca7
MattheusWildschut <125035919+MattheusWildschut@users.noreply.github.com>
2023-07-26 18:07:49 +0200

    - Plot Netosis analyses ScanR - Neural network image cropping

--- Netosis/Preprocess_IncucyteImages_TW.R
+++ Netosis/Preprocess_IncucyteImages_TW.R
@@ -37,7 +37,8 @@ walk(as.list(files), function(filename){
 
 ## New version ------------------------------------------------------------------------------------------
 # in.folder = "C:/Users/mwildschut/OneDrive - Vifor Pharma AG/Documents/Incucyte/Netosis/Netosis_Exp12/Raw_Images_Renamed/"
-in.folder = "F:/RPTEC3_Injury_Cytotox_Renamed/"
+# in.folder = "E:/Netosis_Exp12/Renamed_Images"
+in.folder = "C:/Users/mwildschut/OneDrive - Vifor Pharma AG/Documents/Incucyte/Netosis/Netosis_Exp10/Renamed_Images"
 files = list.files(in.folder)
 mat96 = expand.grid(LETTERS[1:8], 1:12)
 mat96 = matrix(paste0(mat96$Var1, mat96$Var2), nrow = 8, ncol = 12)
@@ -50,25 +51,30 @@ f = files %>%
          Well = str_extract(Filename, "(?<=_)[A-Z][:digit:]{1,2}"),
          Well2 = match(Well, t(mat96)),
          Field = str_extract(Filename, "(?<=_[A-Z][:digit:]{1,2}_)[:digit:]*"),
-         Time = str_extract(Filename, "[:digit:]{2}d[:digit:]{2}h[:digit:]{2}m")) %>%
-  group_by(Exp, Channel, Well, Field) %>% dplyr::mutate(Time2 = order(Time))
+         Time = str_extract(Filename, "[:digit:]{2}d[:digit:]{2}h[:digit:]{2}m"),
+         Time2 = as.numeric(factor(Time)))
 files.new = paste0(f$Exp, "_", f$Well, "--", "W", fill.n(f$Well2), "--", "P", fill.n(f$Field), "--Z00000--", 
                    "T", fill.n(f$Time2), "--", f$Channel, ".tif")
+# files.new = str_remove(files, "_[:digit:]{2}d[:digit:]{2}h[:digit:]{2}m")
 
-## To rename
-rename = file.rename(paste0(in.folder, files), paste0(in.folder, files.new))
+## To rename -----------------------------------------------------------------------------------------------------------------------
+rename = file.rename(paste0(in.folder, "/", files), paste0(in.folder, "/", files.new))
 paste0(sum(rename), "/", length(files.new))
 
-## To rewrite as 1024px
-map2(as.list(files), as.list(files.new), function(x,y){
+## To rewrite as 1024px ------------------------------------------------------------------------------------------------------
+out.folder = "E:/Netosis_Exp12/Raw_Images_Renamed"
+unlink(out.folder, recursive = TRUE)
+dir.create(out.folder)
+
+walk2(as.list(files), as.list(files.new), function(x,y){
   im = image_read(paste0(in.folder, "/", x), strip = TRUE)[1]
   im = image_scale(im, "x1024")
   image_write(im, path = paste0(out.folder, "/", y), format = "tiff", depth = 16)
-  print(paste0(y, " written to disk (", which(filename == files), "/", length(files), ")"))
+  print(paste0(y, " written to disk (", which(x == files), "/", length(files), ")"))
 })
 
 
-
+## Trial multichannel TIFF -------------------------------------------------------------------------------------
 TIFFWriteDirectoryTagData
 magick:::magick_image_write()
 
@@ -118,32 +124,132 @@ img.function(im.c, c("Red", "Green", "Grey"))
 
 im.f = image_flatten(im.c)
 
-## 
-data.class = fread(paste0(folder, "ParameterData_Classification.txt")) %>%
+## Analyze & plot Netosis data -----------------------------------------------------------------------------
+folder = "C:/Users/mwildschut/OneDrive - Vifor Pharma AG/Documents/Incucyte/Netosis/Netosis_Exp12/Tables_ScanR"
+data.class = fread(paste0(folder, "/All_NET-NN_Images-Converted4_NET_NN.txt")) %>%
   arrange(Area)
-data.main = fread(paste0(folder, "ParameterData_Main.txt"))
+data.main = fread(paste0(folder, "/All_NET-NN_Images-Converted4_Main.txt"))
+conditions = fread("C:/Users/mwildschut/OneDrive - Vifor Pharma AG/Documents/Incucyte/Netosis/Netosis_Exp12/Conditions_Netosis-Exp12.csv") %>%
+  mutate(Condition = str_remove(paste(Stimuli, Inhibitor, sep = "|"), "\\|$|^\\|")) %>%
+  arrange(rank(as.numeric((str_extract(Inhibitor, "[:digit:]*"))), na.last = FALSE)) %>%
+  arrange(rank(as.numeric((str_extract(Stimuli, "[:digit:]*"))), na.last = FALSE))
+cond = unique(conditions$Condition)
 
 mat96 = expand.grid(LETTERS[1:8], 1:12)
 mat96 = matrix(paste0(mat96$Var1, mat96$Var2), nrow = 8, ncol = 12)
+classes = c("NET", "SmallGreen", "Original", "FlatCell")
+timepoints = paste0(rep(0:9, each = 2), c("h00m", "h30m"))
 
 data.merged = data.main %>%
   left_join(data.class, by = join_by("Object ID" == "Parent Object ID (MO)"), multiple = "first") %>%
-  mutate(Well = t(mat96)[`Parent Object ID (Well)`+1],
-         Class = c("NET", "SmallGreen", "Original", "FlatCell")[`Most frequent class Classification`]) %>%
-  filter(!is.na(Class)) %>%
-  group_by(Well, Class) %>% dplyr::summarise(N = n()) %>%
-  group_by(Well) %>% dplyr::mutate(Frequency = N/sum(N))
+  # filter(Time>2) %>%
+  mutate(Well = factor(t(mat96)[`Parent Object ID (Well)`+1], levels = as.vector(t(mat96))),
+         Time2 = timepoints[Time]) %>%
+  rowwise() %>% 
+  dplyr::mutate(Max_prob = max(c(`Mean Intensity NET`, `Mean Intensity SmallGreen`, `Mean Intensity Original`, `Mean Intensity FlatCells`)),
+                Class = ifelse(Max_prob == 0, 0, 
+                               which(Max_prob == c(`Mean Intensity NET`, `Mean Intensity SmallGreen`, `Mean Intensity Original`, `Mean Intensity FlatCells`))),
+                Class2 = factor(ifelse(Class == 0, "No class", classes[Class]), levels = c(classes, "No class"))) %>%
+  filter(!is.na(Class2))
+data.sum = data.merged %>%
+  mutate(Condition = factor(conditions$Condition[match(Well, conditions$Well)], levels = cond),
+         Class3 = factor(Class2, levels = c(classes, "No class"), labels = c("NET", "NET", "Original", "Original", "No class"))) %>%
+  group_by(Condition, Well, Time2, Class2) %>% dplyr::summarise(N = n()) %>%
+  group_by(Condition, Well, Time2) %>% dplyr::mutate(Percentage = round(N/sum(N)*100,1))# %>%
+  # filter(!str_detect(Condition, "GSK"))
 
-ggplot(data.merged, aes(x = Class, y = Frequency, fill = Class)) +
-  geom_col() +
-  facet_wrap(.~Well, ncol = 3, dir = "v") +
-  theme_bw() + scale_y_continuous(expand = expansion(mult = c(0,0.05)), labels = scales::percent) +
-  theme(axis.text.x = element_blank(), axis.ticks.x = element_blank()) + labs(x = NULL)
+data.sum3 = data.sum %>%
+  group_by(Condition, Time2, Class2) %>% dplyr::summarise(Percentage = round(mean(Percentage),1), N = sum(N))
+# ggplotly(
+a = as.character(unique(data.sum3$Condition))
+             
+  ggplot(data.sum3, aes(x = Condition, y = Percentage, fill = Class2)) +
+    geom_col(position = position_stack(), col = "white", lwd = 0.1) +
+    geom_vline(xintercept = c(7, 13), linetype = 2) +
+    facet_wrap(.~Time2, dir = "h", ncol = 5) +
+    scale_y_continuous(expand = expansion(mult = c(0,0)), labels = ~paste0(.x, "%")) +
+    scale_fill_manual(values = c("NET" = "green3", "SmallGreen" = "green4", "Original" = "red4", "FlatCell" = "red1", "No class" = "grey")) +
+    theme_bw() + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5, size = 8), panel.grid = element_blank()) +
+    labs(fill = "Class", x = NULL) +
+    scale_x_discrete(limits = c(a[1:6], "", a[7:11], "", a[12:16]))
+  # )
 
+celltypes = c("NET", "SmallGreen")#c("FlatCell") #c("Original")#, "FlatCell") #c("NET", "SmallGreen")
+data.sum2 = filter(data.sum, Class2 %in% celltypes)
+# ggplotly(
+plot.list = map(list("Medium", "PMA", "Ionomycin"), function(x){
+  if(x == "Medium"){
+    data.plot = data.sum %>% filter(!str_detect(Condition, "PMA|Ionomycin"))
+  } else {
+    data.plot = data.sum %>% filter(str_detect(Condition, x))
+  }
+  data.plot = data.plot %>% 
+    filter(Class2 %in% celltypes & Condition != "20µM CMP") %>% 
+    dplyr::mutate(Title = x, 
+                  Condition = factor(Condition, levels = levels(Condition), 
+                                     labels = str_replace(str_remove(levels(Condition), "5µM Ionomycin\\||100nM PMA\\|"),
+                                                          "5µM Ionomycin|100nM PMA", "Medium")))
+  ggplot(data.plot, aes(x = Time2, y = Percentage, col = Condition, group = Condition)) +
+    # stat_summary(fun.data = mean_sdl, geom = "errorbar") + #fun.args = list(mult = 1),
+    stat_summary(fun.y = ~sum(.x)/length(.x)*length(celltypes), geom = "line") +
+    theme_bw() + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
+                       plot.title = element_text(hjust = 0.5, colour = "grey")) +
+    facet_grid(.~Title) +
+    scale_y_continuous(limits = c(0,85), expand = expansion(mult = c(0,0)), labels = ~paste0(.x, "%")) +
+    labs(x = NULL, y = ifelse(x == "Medium", "Percentage NETs", ""))
+})
+wrap_plots(plot.list, nrow = 1, guides = "collect")  
+# )
 
-ggplotly(ggplot(data.img, aes(x = Timepoint, y = NET_percentage, col = Concentration, group = Concentration)) +
-           stat_summary(fun.data = mean_sdl, geom = "errorbar") + #fun.args = list(mult = 1),
-           stat_summary(fun.y = mean, geom = "line") +
-           # geom_line() +
-           facet_grid(.~Condition) +
-           scale_color_viridis_d() + theme_bw() + theme(axis.text.x = element_text(angle = 90, vjust = 0.5, hjust = 1)))
+data.plot = data.sum %>%
+  filter(Class2 %in% celltypes & Condition != "20µM CMP") %>%
+  dplyr::mutate(Stimuli = factor(Condition, levels = levels(Condition),
+                                   labels = replace_na(str_extract(levels(Condition), "PMA|Ionomycin"), "No stimuli")),
+                Inhibitor = factor(Condition, levels = levels(Condition),
+                                 labels = replace_na(str_extract(levels(Condition), "JBI_589|GSK484"), "GSK484")),
+                Concentration = as.numeric(replace_na(str_extract(Condition, "62.5|125|250|500"), "0")))
+ggplot(data.plot, aes(x = Time2, y = Percentage, col = Concentration, group = Concentration)) +
+  # stat_summary(fun.data = mean_sdl, geom = "errorbar") + #fun.args = list(mult = 1),
+  stat_summary(fun.y = ~sum(.x)/length(.x)*length(celltypes), geom = "line") +
+  theme_bw() + theme(axis.text.x = element_text(angle = 90, hjust = 1, vjust = 0.5),
+                     plot.title = element_text(hjust = 0.5, colour = "grey"), panel.grid = element_blank()) +
+  facet_grid(Stimuli~Inhibitor) +
+  scale_y_continuous(limits = c(0,82), expand = expansion(mult = c(0,0)), labels = ~paste0(.x, "%")) +
+  scale_color_viridis(option = "H") +
+  labs(x = NULL, "Percentage NETs")
+
+## Analysis Exp10 -------------------------------------------------------------------------------------------------------------------
+folder = "C:/Users/mwildschut/OneDrive - Vifor Pharma AG/Documents/Incucyte/Netosis/Netosis_Exp10/Tables_ScanR"
+data.class = fread(paste0(folder, "/Netosis_Exp10_NET_NN.txt")) %>%
+  arrange(Area)
+data.main = fread(paste0(folder, "/Netosis_Exp10_Main.txt"))
+# conditions = fread("C:/Users/mwildschut/OneDrive - Vifor Pharma AG/Documents/Incucyte/Netosis/Netosis_Exp12/Conditions_Netosis-Exp12.csv") %>%
+#   mutate(Condition = str_remove(paste(Stimuli, Inhibitor, sep = "|"), "\\|$|^\\|")) %>%
+#   arrange(rank(as.numeric((str_extract(Inhibitor, "[:digit:]*"))), na.last = FALSE)) %>%
+#   arrange(rank(as.numeric((str_extract(Stimuli, "[:digit:]*"))), na.last = FALSE))
+conditions = fread(paste0(folder, "/PlateMap_Exp10.csv")) %>%
+  mutate(Condition2 = paste(Condition, Concentration, sep = "_")) %>%
+  arrange(Condition, Concentration)
+cond = unique(conditions$Condition2)
+
+mat96 = expand.grid(LETTERS[1:8], 1:12)
+mat96 = matrix(paste0(mat96$Var1, mat96$Var2), nrow = 8, ncol = 12)
+classes = c("NET", "SmallGreen", "Original", "FlatCell")
+timepoints = paste0(rep(0:9, each = 2), c("h00m", "h30m"))
+
+data.merged = data.main %>%
+  left_join(data.class, by = join_by("Object ID" == "Parent Object ID (MO)"), multiple = "first") %>%
+  # filter(Time>2) %>%
+  mutate(Well = factor(t(mat96)[`Parent Object ID (Well)`+1], levels = as.vector(t(mat96))),
+         Time2 = timepoints[Time]) %>%
+  rowwise() %>% 
+  dplyr::mutate(Max_prob = max(c(`Mean Intensity NET`, `Mean Intensity SmallGreen`, `Mean Intensity Original`, `Mean Intensity FlatCells`)),
+                Class = ifelse(Max_prob == 0, 0, 
+                               which(Max_prob == c(`Mean Intensity NET`, `Mean Intensity SmallGreen`, `Mean Intensity Original`, `Mean Intensity FlatCells`))),
+                Class2 = factor(ifelse(Class == 0, "No class", classes[Class]), levels = c(classes, "No class"))) %>%
+  filter(!is.na(Class2))
+data.sum = data.merged %>%
+  mutate(Condition = factor(conditions$Condition2[match(Well, conditions$Well)], levels = cond),
+         Class3 = factor(Class2, levels = c(classes, "No class"), labels = c("NET", "NET", "Original", "Original", "No class"))) %>%
+  group_by(Condition, Well, Time2, Class2) %>% dplyr::summarise(N = n()) %>%
+  group_by(Condition, Well, Time2) %>% dplyr::mutate(Percentage = round(N/sum(N)*100,1))# %>%
